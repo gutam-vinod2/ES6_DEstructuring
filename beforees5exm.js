6034e606b38fd0a52138158b37446f6214df1c78
[firstname lastname] <[valid-email]>
2022-08-30 15:31:01 +0530

    destructuring -es6

---
+++ beforees5exm.js
@@ -0,0 +1,17 @@
+// assigning object values to avriables
+
+const person ={
+    name: "vinod",
+    age : "23",
+    gender :"male"
+}
+
+let name = person.name;
+let age = person.age;
+let gender = person.gender;
+
+
+console.log(name);
+console.log(age);
+console.log(gender);
+
---
+++ index.js
@@ -0,0 +1,14 @@
+// assigning object attributes to variables
+
+const person ={
+    name: "vinod",
+    age : "23",
+    gender : "male"
+}
+
+// destructuring assignment
+let {name,age,gender} = person;
+
+console.log(name);
+console.log(age);
+console.log(gender);
